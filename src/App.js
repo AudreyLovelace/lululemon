69e1ff5eed2ccc80d0657638464724878f368dad
audreylovelace <yueyueaudrey2021@gmail.com>
2023-03-14 10:49:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 function App() {
-<<<<<<< Updated upstream
-  return <div className="App">aaa</div>;
-=======
   return <div className="App">aaaa</div>;
->>>>>>> Stashed changes
 }
 
 export default App;
