e723314bf0d99d6817437e53d25137fce9031474
audreylovelace <yueyueaudrey2021@gmail.com>
2023-03-14 11:05:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      aaa<span>bbb</span>
-      <span>ccc</span>
-      <span>ddd</span>
-    </div>
-  );
+  return <div className="App">aaa</div>;
 }
 
 export default App;
