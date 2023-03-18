f7f5d6b3a19cf3e56c5db41bb615cb423529a4ed
Connectvv <grandslamvv@gmail.com>
2023-03-17 23:04:40 -0700

    Feedback
    input basic information of Feedback component

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,32 +1,32 @@
 import './feedback.scss';
 import React, {useState} from "react";
-/*import StarIcon from '@mui/icons-material/Star'*/
+import {red} from "@mui/material/colors";
+// import StarIcon from '@mui/icons-material/Star'
 
 const Feedback = () => {
     const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     return (
 
-        <div>
+        <div className='feedback'>
             <img
                 src="https://resources.digital-cloud.medallia.com/wdcus/55284/resources/image/1587642806364_rsz_lululemon_yogo_rgb_clr-54.jpg"
                 alt="logo"/>
 
-            <div>
+            <div className='close'>
                 close x
             </div>
             <div>
-                <h4>We're listening!</h4>
-                <p>We love to sweat the details. Tell us what you think.</p>
-                <br/>
+                <h5>We're listening!</h5>
+                <h6>We love to sweat the details. Tell us what you think.</h6>
                 <br/>
             </div>
 
             <div>
-                <p>Overall, how satisfied are you with your experience?
-                    //todo
-                    <span>*</span> //scss: margin top -10
-                </p>
+                <h6>Overall, how satisfied are you with your experience?
+                    {/*todo*/}
+                    <span className="asterisk">*</span> {/*scss: margin top -10*/}
+                </h6>
 
             </div>
             <div>
@@ -36,11 +36,10 @@ const Feedback = () => {
                     );
                 })}
                 <br/>
-                <br/>
             </div>
 
             <div>
-                <p>How likely are you to recommend lululemon to a friend or family member?</p>
+                <h6>How likely are you to recommend lululemon to a friend or family member?</h6>
             </div>
 
             <div>
@@ -52,9 +51,34 @@ const Feedback = () => {
                     );
                 })}
                 <br/>
-                <br/>
             </div>
 
+            <div>
+                <h6>Please tell us about your experience.</h6>
+                <input type="text"/>
+            </div>
+
+            <div>
+                <h6>Would you like us to follow up with you?</h6>
+                <input type="radio" value='Yes' name='Yes'/> Yes
+                <input type="radio" value='No' name='No'/> No
+            </div>
+
+            <div>
+                <h6>We really appreciate your feedback. Have questions or need immediate support? Click here and contact the Guest Education Center.
+                </h6>
+                <br/>
+                <h6>Your responses will be used in accordance with our privacy policy</h6>
+            </div>
+
+            <div>
+                <button>Close</button>
+                <button>Submit</button>
+            </div>
+
+            <div>
+                <h6>Powered by Medallia</h6>
+            </div>
         </div>
     )
 }
