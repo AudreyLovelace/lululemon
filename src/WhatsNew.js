a3340eedf3fc0087d1fb2566207ebb78ccdc7ed5
kakazhouD <127874233+kakazhouD@users.noreply.github.com>
2023-03-20 20:06:16 -0400

    1

--- src/WhatsNew.js
+++ src/WhatsNew.js
@@ -24,6 +24,7 @@ export default function WhatsNew(props) {
             <nav>
                 <div className="container">
                     <TopNavigation/>
+                    {/*<DropDown/>*/}
                 </div>
             </nav>
             <div className="container">
